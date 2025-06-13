a7de72949e10df2528ac5260335cac2eedccb261
Mrudula230 <mrudula.sur2005@gmail.com>
2025-06-13 18:34:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the saved model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Get data from form
+    input_features = [float(x) for x in request.form.values()]
+    features_array = np.array([input_features])
+    prediction = model.predict(features_array)
+    output = round(prediction[0], 2)
+    return render_template("index.html", prediction_text=f"Predicted House Price: ${output}k")
+
+if __name__ == "__main__":
+    app.run(debug=True)
